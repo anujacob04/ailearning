5e36039b035ade23a2b6f16c8b5dd069236d36c0
anujacob04 <anujacob04@gmail.com>
2026-02-13 20:10:31 +0530

    initial changes

---
+++ src/test/java/com/example/aichatdemo/AichatdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aichatdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AichatdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
